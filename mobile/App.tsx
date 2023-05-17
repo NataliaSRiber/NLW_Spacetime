e8c0ab5c238e45035395a7ae4637cdf9823fcef5
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-17 17:06:26 -0300

    feat: it modifies files to use nativewind

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-gray-950 flex-1 items-center'>
+      <Text className='text-gray-50 font-bold text-5xl'>Testing!</Text>
+      <StatusBar style="light" translucent/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-2xl text-pink-500">Hello World!</h1>
+  return <h1 className="bg-slate-950 text-2xl text-pink-500">Hello World!</h1>
 }
