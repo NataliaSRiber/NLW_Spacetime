54c83bbd92d3937c5b906590a7667f576a0f5ed2
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-17 17:15:27 -0300

    styles: itremoves spaces

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className='bg-gray-950 flex-1 items-center'>
-      <Text className='text-gray-50 font-bold text-5xl'>Testing!</Text>
-      <StatusBar style="light" translucent/>
+    <View className="bg-gray-950 flex-1 items-center">
+      <Text className="text-gray-50 font-bold text-5xl">Testing!</Text>
+      <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
