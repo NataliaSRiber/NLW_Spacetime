d1cbc9cabbf4e27e2ab83600f43626ef27d1154d
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-18 16:07:21 -0300

    feat: it finishes home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,6 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwlogo from '../assets/nlw-spacetime-logo.svg'
 
 export default function Home() {
   return (
@@ -22,6 +24,37 @@ export default function Home() {
             memórias!
           </p>
         </a>
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image alt="NLX Spacetime logo" src={nlwlogo} />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANçA
+          </a>
+        </div>
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            className="underline hover:text-gray-100"
+            href="https://www.rocketseat.com.br/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
       {/* right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] p-16">
