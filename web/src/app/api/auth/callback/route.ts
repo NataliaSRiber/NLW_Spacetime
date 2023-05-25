f714f3b665f180267b17b6d15da6d5e9dae94f12
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-25 10:10:40 -0300

    feat: it saves the token in cookies

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,25 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code') // code from github
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  // redirects the user to /
+  const redirectURL = new URL('/', request.url)
+
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+
+  return NextResponse.redirect(redirectURL, {
+    // saving the token information on cookie, path=/ all app can access the token
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+    },
+  })
+}
