0061f65d5c2a0ffa8907a167443443c5965e1e8c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-26 10:17:41 -0300

    feat: it adds profile page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Copyright from '@/components/Copyright'
 import EmptyMemories from '@/components/EmptyMemories'
 import Hero from '@/components/Hero'
+import Profile from '@/components/Profile'
 import SignIn from '@/components/SignIn'
+import { cookies } from 'next/dist/client/components/headers'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* left */}
@@ -12,7 +16,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
         {/* stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
