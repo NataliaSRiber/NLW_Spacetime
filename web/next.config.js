92130ffe8a909a165188c50b2e5ec990a98b1d45
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-26 10:17:11 -0300

    feat: it adds github avatar to nextjs accept to use it

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // you add domain for nextjs accept to use an external image
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+}
 
 module.exports = nextConfig
