ec07909e7cc7f384661c24af87c802543931289f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-30 14:08:15 -0300

    feat: it adds logout route on profile

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -13,7 +13,10 @@ export default function Profile() {
         className="h-10 w-10 rounded-full"
       />
       <p className="max-w-[140px] text-sm leading-snug">{name}</p>
-      <a href="" className="block text-red-400 hover:text-red-300">
+      <a
+        href="/api/auth/logout"
+        className="block text-red-400 hover:text-red-300"
+      >
         Sair
       </a>
     </div>
