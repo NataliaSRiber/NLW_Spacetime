b879b60df1d34096fd3632b7b002409d67f4064e
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-05-31 15:17:48 -0300

    feat: it adds switch bottom on new memory page

---
+++ mobile/app/new.tsx
@@ -0,0 +1,42 @@
+import { Text, Switch, TouchableOpacity, View } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useState } from 'react'
+
+export default function NewMemories() {
+  // defines top for different types of mobiles
+  const { bottom, top } = useSafeAreaInsets()
+  // save the switch status on state
+  const [isPublic, setIsPublic] = useState(false)
+
+  return (
+    <View
+      className="flex-1 px-8"
+      style={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#fff" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            trackColor={{ false: '#767577', true: '#372560' }}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memória pública
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
