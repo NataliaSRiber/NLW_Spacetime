2a3435c4f45f371d27fb321f1c8b8ea669429ec8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-06-05 14:35:26 -0300

    style: it removes console.log

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -21,13 +21,13 @@ export async function uploadRoutes(app: FastifyInstance) {
     // to know if the upload is a video or image
     const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
     const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
-
     if (!isValidFileFormat) {
       return reply.status(400).send()
     }
 
     const fileId = randomUUID()
     const extension = extname(upload.filename)
+
     // it adds a random number at the end of the file name to avoid files with same name
     const fileName = fileId.concat(extension)
     // to save the upload slowly and where to save it
@@ -39,7 +39,6 @@ export async function uploadRoutes(app: FastifyInstance) {
     // creates a url to access files
     const fullUrl = request.protocol.concat('://').concat(request.hostname)
     const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
-    // console.log(fileUrl)
     return { fileUrl }
   })
 }
