7b7848dc0d1cb4a39fffcc64c5b10fca5b50a13c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-06-05 14:33:17 -0300

    refactor: it adds coerce to recognize boolean

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -48,7 +48,8 @@ export async function memoriesRoutes(app: FastifyInstance) {
     const bodySchema = z.object({
       content: z.string(),
       coverUrl: z.string(),
-      isPublic: z.boolean().default(false),
+      // isPublic: z.boolean().default(false),
+      isPublic: z.coerce.boolean().default(false),
     })
     const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
     const memory = await prisma.memory.create({
