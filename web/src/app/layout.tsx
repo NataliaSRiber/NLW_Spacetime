4a0dd12d4e5618d9fd7a284cdb04eda2aeb36f1a
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-06-06 12:54:39 -0300

    feat: it modifies layoult to have scroll only on right page

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <Copyright />
           </div>
           {/* right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)]">
             {children}
           </div>
         </main>
